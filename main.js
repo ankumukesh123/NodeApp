32998752c1337c4e3c19ff599ea8040e8de5bbf0
Mukesh <ankumukesh.thakur@gmail.com>
2019-11-22 06:50:48 +0000

    test

--- main.js
+++ main.js
@@ -4,11 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-<<<<<<< HEAD
   response.end("Congratulation, Succesfully Deployed Nodejs Application \n");
-=======
-  response.end("Congratulation, Succesfully Deployed Nodejs App \n");
->>>>>>> 2255d24ce7168e12816dfb8abe0d0a3a5c8f2ac2
 });
 
 // listen on localhost:8000
